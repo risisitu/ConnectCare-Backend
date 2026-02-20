55c946ee0814815179e32a38802408ba72e59792
Risittan <risittan@gmail.com>
2026-02-20 11:46:25 +0000

    added consultation timeslots

--- src/app.js
+++ src/app.js
@@ -67,6 +67,7 @@ app.use('/api/patients', patientRoutes);
 app.use('/api/appointments', appointmentRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/availability', require('./routes/availability.routes'));
 app.use('/api', reportRoutes); // Routes inside report.routes.js already have /appointments/... or /reports/...
 
 
